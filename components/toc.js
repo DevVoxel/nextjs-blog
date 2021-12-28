79340dfba8a54f351847be8589c2119728eef515
voxel20 <aidensmith20@gmail.com>
2021-12-27 22:35:52 -0500

    getStaticProps func removed from TOC component

--- components/toc.js
+++ components/toc.js
@@ -4,15 +4,6 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-      props: {
-        allPostsData
-      }
-    }
-  }
-
 export default function TOC({ allPostsData }) {
     return (
         <aside className={utilStyles.asideBox}>
