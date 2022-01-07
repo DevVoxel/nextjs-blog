69d825157809ab546ac285fc491a92c38829f2af
voxel20 <aidensmith20@gmail.com>
2022-01-06 22:04:09 -0500

    Useless Head Removed.

--- components/hovercard.js
+++ components/hovercard.js
@@ -11,34 +11,24 @@ export const siteTitle = "Voxel's Blog";
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Voxel's personal blog website." />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
       <div className={hovercard.card}>
-      <div classname={hovercard.cardcontent}>
-        <h2 className={hovercard.cardtitle}>Random Title</h2>
-        <p className={hovercard.cardbody}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-          labore consequatur illo in quidem!
-        </p>
-        <a href="#" className={hovercard.button}>Learn More</a>
-      </div>
-    </div>
-
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
+        <div classname={hovercard.cardcontent}>
+          <h2 className={hovercard.cardtitle}>Random Title</h2>
+          <p className={hovercard.cardbody}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+            labore consequatur illo in quidem!
+          </p>
+          <a href="#" className={hovercard.button}>
+            Learn More
+          </a>
         </div>
+      </div>
+
+      <div className={styles.backToHome}>
+        <Link href="/">
+          <a>← Back to home</a>
+        </Link>
+      </div>
     </div>
   );
 }
