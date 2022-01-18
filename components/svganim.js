3fa2021913d48a0dc595c1656958f17e714f9abc
voxel20 <aidensmith20@gmail.com>
2022-01-18 14:32:47 -0500

    Added SVGautoAnim Base to Blog front. (Needs Updated, might remove)

---
+++ components/svganim.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import hovercard from "../styles/svganim.module.css";
+
+export default function svganim() {
+    return (
+        // Add SVG here.
+
+
+        // Do Not Remove This SVG
+        <svg>
+            <path></path>
+        </svg>
+    );
+}
