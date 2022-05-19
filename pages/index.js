71d1c25a1f066bae81fc122a2efe791a9504b381
Voxel20 <aidensmith20@gmail.com>
2022-05-18 23:24:31 -0400

    Styling Change.

--- pages/index.js
+++ pages/index.js
@@ -45,7 +45,7 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
       <br/>
-      <p className={`${utilStyles.uselessCenter}`}>Sidenote: Every post has a French translation! Press "FR" to view!</p>
+      <p className={`${utilStyles.uselessCenter}`}>Sidenote: Every post has a French translation! Press "<strong>FR</strong>" to view!</p>
     </Layout>
   );
 }
